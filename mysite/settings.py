680857f54a592fa955ddba7d4e7943d738d7ba69
JOSHUA DRAGE <j.drage.21@abdn.ac.uk>
2022-04-27 18:50:32 +0000

    Creation of Template Framework and basic pages

--- mysite/settings.py
+++ mysite/settings.py
@@ -55,7 +55,9 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'),
+                 os.path.join(BASE_DIR, 'shopApp', 'templates', 'shopApp'),
+                 ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- shopApp/views.py
+++ shopApp/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import gameList, gameDetails
 
 # Create your views here.
+def index(request):
+    games = gameList.objects.all()
+    return render(request, "index.html", {'games' : games})
+
+def details(request):
+    if request.method == "POST":
+        selection = request.POST.get('Selection','')
+        gDetail  = gameDetails.objects.get(gameID=selection)
+        gSelect = gameList.objects.get(gameID=selection)
+        gName = gSelect.gameName
+        return render(request, "details.html", {'gDetail' : gDetail, 'gName' : gName})
+    return render(request, "details.html")
