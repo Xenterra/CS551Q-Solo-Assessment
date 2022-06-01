4cfc2005dd0455e472c76c39e53ee46a25c5b06e
JOSHUA DRAGE <j.drage.21@abdn.ac.uk>
2022-06-01 17:17:13 +0000

    New Project Build

--- .venv/bin/activate.fish
+++ .venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/codio/workspace/.venv"
+set -gx VIRTUAL_ENV "/home/codio/workspace/BackupRetry/.venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
--- shopApp/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from . import models
-
-# Register your models here.
-admin.site.register(models.gameList)
-admin.site.register(models.gameDetails)
-admin.site.register(models.shoppingCart)
-admin.site.register(models.orders)
